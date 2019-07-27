dec186fe0d44441ecda5f2f20ea542e46dcaca44
Cyhyraeth <maboose@yahoo.com>
2019-07-27 14:34:29 -0700

    Fix prompt to only allow correct values

--- script.js
+++ script.js
@@ -5,37 +5,45 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let caseInsensitivePlayerSelection;
     let lowerCasePlayerSelection = playerSelection.toLowerCase();
-    let caseInsensitivePlayerSelection = lowerCasePlayerSelection.replace(lowerCasePlayerSelection[0], lowerCasePlayerSelection[0].toUpperCase());
+    if (lowerCasePlayerSelection === 'rock' || lowerCasePlayerSelection === 'paper' || lowerCasePlayerSelection === 'scissors') {
+        caseInsensitivePlayerSelection = lowerCasePlayerSelection.replace(lowerCasePlayerSelection[0], lowerCasePlayerSelection[0].toUpperCase());
+    } else {
+        alert('Error. You must enter rock, paper, or scissors.')
+    }    
     if (caseInsensitivePlayerSelection === 'Rock') {
         if (computerSelection === 'Rock') {
             console.log(`This round is a tie! Both players chose rock.`)
+            return 'tie'
         } else if (computerSelection === 'Paper') {
             console.log(`You lost this round! Paper covers rock.`)
-            return 0
+            return 'lost'
         } else if (computerSelection === 'Scissors') {
             console.log(`You won this round! Rock crushes scissors.`)
-            return 1
+            return 'won'
         }
     } else if (caseInsensitivePlayerSelection === 'Paper') {
         if (computerSelection === 'Rock') {
             console.log(`You won this round! Paper covers rock.`)
-            return 1
+            return 'won'
         } else if (computerSelection === 'Paper') {
             console.log(`This round is a tie! Both players chose paper.`)
+            return 'tie'
         } else if (computerSelection === 'Scissors') {
             console.log(`You lost this round! Scissors cuts paper.`)
-            return 0
+            return 'lost'
         }
     } else if (caseInsensitivePlayerSelection === 'Scissors') {
         if (computerSelection === 'Rock') {
             console.log(`You lost this round! Rock crushes scissors.`)
-            return 0
+            return 'lost'
         } else if (computerSelection === 'Paper') {
             console.log(`You won this round! Scissors cuts paper.`)
-            return 1
+            return 'won'
         } else if (computerSelection === 'Scissors') {
             console.log(`This round is a tie! Both players chose scissors.`)
+            return 'tie'
         }
     }
 }
@@ -43,17 +51,20 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Please enter your selection (Rock, Paper, or Scissors)');
+    for (let i = 0; i < 5;) {
+        let playerSelection = prompt('Please enter rock, paper, or scissors.');
         let computerSelection = computerPlay();
         let result = playRound(playerSelection, computerSelection);
-        if (result === 0) {
+        if (result === 'lost') {
+            i++;
             computerScore++;
             console.log(`Player: ${playerScore}, Computer: ${computerScore}.`)
-        } else if (result === 1) {
+        } else if (result === 'won') {
+            i++;
             playerScore++;
             console.log(`Player: ${playerScore}, Computer: ${computerScore}.`)
-        } else {
+        } else if (result === 'tie') {
+            i++;
             console.log(`Player: ${playerScore}, Computer: ${computerScore}.`)
         }
     }
